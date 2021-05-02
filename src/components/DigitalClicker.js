2efe871a394d972cc1a3528468e510b3ba939c27
Sean Ngo <seanzngo@gmail.com>
2021-05-01 21:33:03 -0400

    commit tree 062f4342767ef98fd60566b42e8bb7c50cde9e7c

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,22 @@
 // Code DigitalClicker Component Here
+import React, { Component} from 'react' 
+
+
+export default class DigitalClicker extends Component {
+
+    state = {
+        timesClicked: 0
+    }
+
+    incrementClicks = () => {
+        this.setState({timesClicked: this.state.timesClicked + 1})
+    }
+
+    render(){
+        return(
+            <div>
+                <button onClick={this.incrementClicks}>{this.state.timesClicked}</button>
+            </div>
+        )
+    }
+}
